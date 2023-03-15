7e54586ef507b47f8880d85bb0434b1a022d0229
Daria <maivery@mail.ru>
2023-03-15 13:26:19 +0300

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+void ResultArray(string[] firstArray, string[] secondArray)
+{
+    int count = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+    if(firstArray[i].Length <= 3)
+        {
+        secondArray[count] = firstArray[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+string[] firstArray = new string [5] {"123", "-2", "hello", "world", "Russia"};
+string[] secondArray = new string[firstArray.Length];
+Console.WriteLine( "Первоначальный массив:");
+PrintArray(firstArray);
+ResultArray(firstArray, secondArray);
+Console.WriteLine( "Массив из строк меньше, либо равных трем:");
+PrintArray(secondArray);
